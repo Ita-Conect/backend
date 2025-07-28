4f33b5c61b4154b49e8c267f3a4c68445315826b
anderson-celestino <andersoncelestino@alu.ufc.br>
2025-07-28 08:21:09 -0300

    Apagando rota não usada pelo frontend

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -5,7 +5,6 @@ import { createLoja } from "../http/controller/user/createLoja";
 import { registerempreendedor } from "../http/controller/user/registerempreendedor";
 import { getProdutoById } from "../http/controller/user/getProdutoById";
 import { getTodosProdutos } from "../http/controller/user/getTodosProdutos";
-import { listarProdutosPorCategoria } from "../http/controller/user/listarProdutosPorCategoria";
 import { login } from "../http/controller/user/login";
 import { getLojaByEmpreendedor } from "../http/controller/user/getLojaByEmpreendedor";
 import { getProdutosPorLoja } from "../http/controller/user/getProdutosPorLoja";
@@ -17,7 +16,6 @@ export async function routes(app: FastifyInstance) {
   app.post("/empreendedor", registerempreendedor);
   app.get("/produto/:id", getProdutoById);
   app.get("/produto", getTodosProdutos);
-  app.get("/listarProdutosPorCategoria", listarProdutosPorCategoria);
   app.post('/login', login);
   app.get('/loja/empreendedor/:id', getLojaByEmpreendedor);
   app.get('/produto/loja/:lojaId', getProdutosPorLoja);
